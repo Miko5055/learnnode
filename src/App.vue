86c1479b496f9edacfd11a953d9d6ddfea16a121
Miko5055 <144675460+Miko5055@users.noreply.github.com>
2025-01-21 10:23:56 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+import { ref } from "vue";
+
+let message = ref("Gooner");
+let items = ref(['Sai','Piim','Viin','Õlu']);
+
+
+function addItem(){
+    if(message.value.trim() !==''){
+    items.value.push(message.value.trim());
+    }
+    message.value = '';
+
+}
+
+
+</script>
+
+<template>
+    <div class="container mt-2">
+        <div class="field has-addons">
+            <div class="control">
+                <input class="input" type="text" v-model="message" @keypress.enter="addItem">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="addItem">
+                    Add item
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <h3>All Items</h3>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
+        </div>
+    </div>
+
+</template>
+
+<style></style>
